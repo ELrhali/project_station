9877b967ea3dd267f3460aa77c998b8f533ea047
ELrhali <mohammed.elrhali@etu.uae.ac.ma>
2024-02-17 02:22:19 +0100

    V3.02

--- backend_SpringBoot/users/src/main/java/com/pompe/users/exceptions/EmailExistsException.java
+++
@@ -1,8 +0,0 @@
-package com.pompe.users.exceptions;
-
-public class EmailExistsException extends RuntimeException {
-
-    public EmailExistsException(String message) {
-        super(message);
-    }
-}
